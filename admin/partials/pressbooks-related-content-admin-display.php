d635c1d4b7c107714896437c0fc75eb3e186e107
Nicole <niasan@uv.es>
2017-06-08 11:53:48 +0200

    New puglin

---
+++ admin/partials/pressbooks-related-content-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       Nicole
+ * @since      0.1
+ *
+ * @package    Pressbooks_Related_Content
+ * @subpackage Pressbooks_Related_Content/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-pressbooks-related-content-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       Nicole
+ * @since      0.1
+ *
+ * @package    Pressbooks_Related_Content
+ * @subpackage Pressbooks_Related_Content/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.1
+ * @package    Pressbooks_Related_Content
+ * @subpackage Pressbooks_Related_Content/includes
+ * @author     Nicole <nicoleacuna95@gmail.com>
+ */
+class Pressbooks_Related_Content_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.1
+	 */
+	public static function activate() {
+
+	}
+
+}
