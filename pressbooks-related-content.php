1ea34a8047aac19f7e8adfb4991657035918fa42
Daniil Zhitnitskii <35699225+danzhik@users.noreply.github.com>
2018-08-13 09:45:38 +0200

    Update pressbooks-related-content.php

--- pressbooks-related-content.php
+++ pressbooks-related-content.php
@@ -19,7 +19,7 @@
  * Version:           0.1
  * Author:            Nicole
  * Author URI:        Nicole
- * License:           GPL-2.0+
+ * License:           GPL-3.0
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       pressbooks-related-content
  * Domain Path:       /languages
