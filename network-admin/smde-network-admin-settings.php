37a070dacffe5d5ca0f9f2b6e2c978bfc083feb0
huguespages <huguespages@gmail.com>
2019-07-30 15:35:18 +0200

    update multisite

--- network-admin/smde-network-admin-settings.php
+++ network-admin/smde-network-admin-settings.php
@@ -96,6 +96,7 @@ function smde_add_network_settings() {
 
 			?>
       <?php if ($shares_edu[$key]=='1') {
+        if (isset($_GET['hello'])) {
 
         function runMyFunction8() {
           if (isset($_GET['field_name'])) {
@@ -121,7 +122,6 @@ function smde_add_network_settings() {
           }
 }
 
-if (isset($_GET['hello'])) {
 runMyFunction8();
 //refresh the page
 ?> <meta http-equiv="refresh" content="0;URL=admin.php?page=smd_net_set_page"><?php
